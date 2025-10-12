11c94ad18bd12dd649aa15e0277f25c318092b06
Dorkmaster Flek <dorkmasterflek@gmail.com>
2025-10-12 09:42:18 -0400

    Update new-install.sh
    Wrong sudo spot

--- new-install.sh
+++ new-install.sh
@@ -10,10 +10,10 @@ sudo curl -fsL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
-sudo echo \
+echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 
 # Install packages.
