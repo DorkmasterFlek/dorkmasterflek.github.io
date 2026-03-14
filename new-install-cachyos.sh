f9abe178fd7343a385683c1b23e376c1c3245828
Dorkmaster Flek <dorkmasterflek@gmail.com>
2026-03-14 10:23:28 -0400

    Create new-install-cachyos.sh for user setup
    Adds a script for extra user setup with Fish shell.

---
+++ new-install-cachyos.sh
@@ -0,0 +1,7 @@
+#!/bin/fish
+
+# ******** Extra user setup for myself.
+
+curl -fsL https://raw.githubusercontent.com/DorkmasterFlek/dorkmasterflek.github.io/master/fish_extra >> ~/.config/fish/config.fish
+curl -fsL https://raw.githubusercontent.com/DorkmasterFlek/dorkmasterflek.github.io/master/gitignore_global -o ~/.gitignore_global
+curl -fsL https://raw.githubusercontent.com/DorkmasterFlek/dorkmasterflek.github.io/master/gitconfig -o ~/.gitconfig
