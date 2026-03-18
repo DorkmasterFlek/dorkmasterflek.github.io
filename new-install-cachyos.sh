984e77be34da21cb2fd60141c25b892e52d124c0
Dorkmaster Flek <dorkmasterflek@gmail.com>
2026-03-18 10:27:36 -0400

    Disable directory shortening in fish prompt
    Added a setting to disable directory shortening in the shell prompt.

--- new-install-cachyos.sh
+++ new-install-cachyos.sh
@@ -5,3 +5,6 @@
 curl -fsL https://raw.githubusercontent.com/DorkmasterFlek/dorkmasterflek.github.io/master/fish_extra >> ~/.config/fish/config.fish
 curl -fsL https://raw.githubusercontent.com/DorkmasterFlek/dorkmasterflek.github.io/master/gitignore_global -o ~/.gitignore_global
 curl -fsL https://raw.githubusercontent.com/DorkmasterFlek/dorkmasterflek.github.io/master/gitconfig -o ~/.gitconfig
+
+# Disable directory shortening in the shell prompt.
+set -U fish_prompt_pwd_dir_length 0
